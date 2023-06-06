7fefab2e57c3131cf84aac85fdebfa44239cf481
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-06-06 10:49:09 -0700

    added result variable

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for character in str:
         if ord(character) >= 97 and ord(character) <= 122:
             up = chr(ord(character) - 32)
-            print("{}".format(up), end="")
+            result += "{}".format(up)
         else:
-            print("{}".format(character), end="")
-    print()
+            result += "{}".format(character)
+    print("{}".format(result))
