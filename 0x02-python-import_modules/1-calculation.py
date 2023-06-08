a27a38fe68b12e8c62551fa9ace64cc0a12233c3
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-06-08 07:04:16 -0700

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     """
     program that imports functions from the file calculator_1.py
     does some Maths, and prints the result
     the functions are add sub mul and div
     """
-    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
