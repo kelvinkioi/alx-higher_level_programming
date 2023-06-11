8825776b160aee50b12f831df318a112e03a5038
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-06-10 20:29:02 -0700

    calculator that does basic functions

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    k = len(sys.argv)
+
+    if k != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
