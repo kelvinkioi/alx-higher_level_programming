6fee4d093abeb74f8ebab126d2966d054e218740
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-06-27 13:32:14 -0700

    class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """The representation of a square"""
+    def __init__(self, size=None):
+        """__init__ initializes the size
+        of the square.
+        size, is the size of a square"""
+        self.__size = size
