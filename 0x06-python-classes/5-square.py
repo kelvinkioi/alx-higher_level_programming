7a1d646889ca2cd21e749c55031a1fcf232d95bd
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-06-27 15:12:08 -0700

    prints in stdout the square with the character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """The representation of a square"""
+
+    def __init__(self, size=0):
+        """__init__ initializes the size
+        of the square.
+        size, is the size of the sqaure"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Class method that returns
+        the area of the sqaure"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Retrives the private instance of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A property setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """ prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                print("#" * self.__size)
