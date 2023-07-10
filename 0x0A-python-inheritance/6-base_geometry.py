c89221fbeb47021fc46fe279344d3c86e11fb428
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-07-10 12:56:26 -0700

    Public instance method: def area(self)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """represents a base geometry"""
+
+    def area(self):
+        """returns area"""
+        raise Exception('area() is not implemented')
