f045bda6fb2b7a00a9365543615c4cd959a5955d
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-07-10 12:10:20 -0700

    fixed pycodestyle error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 def is_same_class(obj, a_class):
     """Checks if obj is exactly an instance of a_class
     Args:
-        obj: the object 
+        obj: the object
         a_class: the class to compare with the object
     Returns:
         True if the object is exactly an instance of the
