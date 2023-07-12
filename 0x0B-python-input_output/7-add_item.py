69c0541e6e892d70f297d0f0be1cebb5536b334b
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-07-12 14:04:07 -0700

    script that adds all arguments to a Python list, and then save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that adds all arguments to a Python list
+and then save them to a file"""
+
+
+import sys
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+try:
+    it = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    it = []
+for i in sys.argv[1:]:
+    it.append(i)
+save_to_json_file(it, "add_item.json")
