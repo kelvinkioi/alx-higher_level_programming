a3be655bff652d9d80c319b28cc19bcd2717382d
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-07-17 13:21:30 -0700

    public method def to_dictionary(self)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -148,3 +148,13 @@ class Rectangle(Base):
                 self.y = args[4]
             else:
                 break
+
+    def to_dictionary(self):
+        """ returns the dictionary representation of a Rectangle"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
