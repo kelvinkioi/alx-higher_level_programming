6021b7e797874af1367955dc56f49a27ad1459e3
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-09-13 11:47:22 -0700

    function that returns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  for (let i = 0, j = list.length - 1; i < j; i++, j--) {
+    const temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+  }
+  return list;
+};
