296fc9d4534c9bf59b7d9ff9c270172b0e9d4082
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-09-17 11:47:46 -0700

    script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states ORDER BY states.id")
+    rows = c.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    db.close()
