91fc511c06249722099c8a06870dba095af4a397
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-09-17 12:01:34 -0700

    script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,15 +5,11 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    try:
-        db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect(host="localhost",
                              port=3306,
                              user=sys.argv[1],
                              passwd=sys.argv[2],
                              db=sys.argv[3])
-    except Exception:
-        print('Error')
-        exit()
 
     c = db.cursor()
     c.execute("""SELECT cities.id, cities.name, states.name \
