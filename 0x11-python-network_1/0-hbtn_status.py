37538bbfcc7fcb70b616f3b98d89aeac8ce7893b
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-09-29 09:21:01 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as url:
+    data = url.read()
+print("Body response:")
+print(f"\t- type: {type(data)}")
+print(f"\t- content: {data}")
+print(f"\t- utf8 content: {data.decode('utf-8')}")
+    
